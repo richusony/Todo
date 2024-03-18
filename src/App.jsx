cec0c1199fa3e5a5a809f9dc30d69fb4fc9dcc42
Richu Sony <sonyrichu4@gmail.com>
2024-03-18 19:26:46 +0530

    docs: added config file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Todo from './components/Todo/Todo'
-import Bg from './images/dotBack.jpg'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className={`h-screen bg-[#7BD5F5] flex justify-center items-center`}>
+      <div className={`min-h-screen bg-[#7BD5F5] flex justify-center items-center`}>
         <Todo />
       </div>
     </>
--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -3,19 +3,11 @@ import Todos from "../Todos/Todos";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { data } from "../Todos/data";
+import { months, weekday } from "./config";
 const Todo = () => {
   const [input, setInput] = useState("");
   const [list, setList] = useState(data);
-  const weekday = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
   const d = new Date();
   let day = d.getDay();
   let date = d.getUTCDate();
@@ -23,17 +15,21 @@ const Todo = () => {
   let year = d.getFullYear();
 
   document.title = list[0].task + " => " + "Todo";
-document.title.trim();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(!input) return alert("Enter your task!!")
+    if (!input) return alert("Enter your task!!");
     setList([...list, { id: list[list.length - 1].id + 1, task: `${input}` }]);
     setInput("");
   };
   return (
     <div className="px-8 py-4 min-h-1/2 bg-[#1CA7EC] rounded-xl shadow-2xl">
-      <h1 className="text-center text-white text-2xl font-semibold">{weekday[day]}</h1>
-      <h3 className="text-center text-white text-xl">{date} {months[month]}, {year}</h3>
+      <h1 className="text-center text-white text-2xl font-semibold">
+        {weekday[day]}
+      </h1>
+      <h3 className="text-center text-white text-xl">
+        {date} {months[month]}, {year}
+      </h3>
 
       <div className="my-3 w-full flex justify-between">
         <input
@@ -53,7 +49,7 @@ document.title.trim();
         </button>
       </div>
 
-      <Todos todos={list} setTodos={setList}/>
+      <Todos todos={list} setTodos={setList} />
     </div>
   );
 };
